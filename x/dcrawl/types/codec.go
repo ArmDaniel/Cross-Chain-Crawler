e3f07a47b77ca3d64f1845d53424c7a5f6fe0482
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-16 00:00:29 +0300

    Create codec.go

---
+++ x/dcrawl/types/codec.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+// RegisterCodec registers concrete types on codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgCreateScavange{},"dcrawl/CreateScavange",nil)
+	cdc.RegisterConcrete(MsgCommitSolution{},"dcrawl/CommitSolution",nil)
+	cdc.RegisterConcrete(MsgRevealSolution{},"dcrawl/RevealSolution",nil)
+}
+
+// ModuleCdc defines the module codec
+var ModuleCdc *codec.Codec
+
+func init() {
+	ModuleCdc = codec.New()
+	RegisterCodec(ModuleCdc)
+	codec.RegisterCrypto(ModuleCdc)
+	ModuleCdc.Seal()
+}
