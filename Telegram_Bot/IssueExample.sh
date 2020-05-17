f3650e7cdc21da1a70a93e19d475bccd032a8f4b
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-17 21:02:35 +0300

    Create IssueExample.sh

---
+++ Telegram_Bot/IssueExample.sh
@@ -0,0 +1,23 @@
+scavengeCLI tx dcrawl createScavenge 69foo "How safe is Ethereum?" --from bot
+
+
+scavengeCLI q dcrawl get e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+
+/*
+Example:
+solutionHash is an artefact, it will be modified
+
+{
+  "creator": "cosmos1q0z497ezps254cv76c8702f3nwfu5f8nxheq2r",
+  "description": "How safe is Ethereum?",
+  "solutionHash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+  "reward": [
+    {
+      "denom": "foo",
+      "amount": "69"
+    }
+  ],
+  "solution": "",
+  "scavenger": ""
+}
+*/
