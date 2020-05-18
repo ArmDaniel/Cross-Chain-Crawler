adf68b8ba52dc5877b738390837518b8c4e8b578
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-18 22:29:19 +0300

    Create QueryData.sh

---
+++ Telegram_Bot/QueryData.sh
@@ -0,0 +1,5 @@
+#The Bot runs it in the background upon request, can thus be added in automation workflow
+#The Bot will check the URL and seek it in the scavenges
+
+#Grabs scavenges
+scavengeCLI query dcrawl list
