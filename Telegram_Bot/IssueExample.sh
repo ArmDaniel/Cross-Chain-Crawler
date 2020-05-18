e9deb9a5b458a21ebf816d8257a8fe7f89d30d87
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-18 15:22:10 +0300

    Update IssueExample.sh

--- Telegram_Bot/IssueExample.sh
+++ Telegram_Bot/IssueExample.sh
@@ -1,6 +1,7 @@
-scavengeCLI tx dcrawl createScavenge 69foo "How safe is Ethereum?" --from bot
+//Creates a scavenge(just an example, there can be any url served as input)
+scavengeCLI tx dcrawl createScavenge 69foo "How safe is ethereum.org?" --from bot
 
-
+//lists the scavenge
 scavengeCLI q dcrawl get e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 
 /*
@@ -9,7 +10,7 @@ solutionHash is an artefact, it will be modified
 
 {
   "creator": "cosmos1q0z497ezps254cv76c8702f3nwfu5f8nxheq2r",
-  "description": "How safe is Ethereum?",
+  "description": "How safe is ethereum.org?",
   "solutionHash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
   "reward": [
     {
